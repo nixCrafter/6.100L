d1ea3f07d52bb6fd572281b543444d9249af8a7f
nixCrafter <nixCrafter.dev@gmail.com>
2025-11-17 05:27:54 +0700

    Complete Problem Set 3, Letter Frequencies

--- ps3/document_distance.py
+++ ps3/document_distance.py
@@ -53,10 +53,10 @@ def get_frequencies(input_iterable):
     Note:
         You can assume that the only kinds of white space in the text documents we provide will be new lines or space(s) between words (i.e. there are no tabs)
     """
-    dict = {}
+    freq = {}
     for i in input_iterable:
-        dict[i] = dict[i] + 1 if i in dict else 1
-    return dict
+        freq[i] = freq[i] + 1 if i in freq else 1
+    return freq
 
 
 ### Problem 2: Letter Frequencies ###
@@ -69,7 +69,10 @@ def get_letter_frequencies(word):
         is a letter in word and the corresponding int
         is the frequency of the letter in word
     """
-    pass
+    freq = {}
+    for letter in word:
+        freq[letter] = freq[letter] + 1 if letter in freq else 1
+    return freq
 
 
 ### Problem 3: Similarity ###
@@ -187,22 +190,22 @@ if __name__ == "__main__":
     # print(friend)  # should print ['hello', 'friends']
 
     ## Tests Problem 1: Get Frequencies
-    test_directory = "tests/student_tests/"
-    hello_world, hello_friend = (
-        load_file(test_directory + "hello_world.txt"),
-        load_file(test_directory + "hello_friends.txt"),
-    )
-    world, friend = text_to_list(hello_world), text_to_list(hello_friend)
-    world_word_freq = get_frequencies(world)
-    friend_word_freq = get_frequencies(friend)
-    print(world_word_freq)  # should print {'hello': 2, 'world': 1}
-    print(friend_word_freq)  # should print {'hello': 1, 'friends': 1}
+    # test_directory = "tests/student_tests/"
+    # hello_world, hello_friend = (
+    #     load_file(test_directory + "hello_world.txt"),
+    #     load_file(test_directory + "hello_friends.txt"),
+    # )
+    # world, friend = text_to_list(hello_world), text_to_list(hello_friend)
+    # world_word_freq = get_frequencies(world)
+    # friend_word_freq = get_frequencies(friend)
+    # print(world_word_freq)  # should print {'hello': 2, 'world': 1}
+    # print(friend_word_freq)  # should print {'hello': 1, 'friends': 1}
 
     ## Tests Problem 2: Get Letter Frequencies
-    # freq1 = get_letter_frequencies('hello')
-    # freq2 = get_letter_frequencies('that')
-    # print(freq1)      #  should print {'h': 1, 'e': 1, 'l': 2, 'o': 1}
-    # print(freq2)      #  should print {'t': 2, 'h': 1, 'a': 1}
+    freq1 = get_letter_frequencies("hello")
+    freq2 = get_letter_frequencies("that")
+    print(freq1)  #  should print {'h': 1, 'e': 1, 'l': 2, 'o': 1}
+    print(freq2)  #  should print {'t': 2, 'h': 1, 'a': 1}
 
     ## Tests Problem 3: Similarity
     # test_directory = "tests/student_tests/"
