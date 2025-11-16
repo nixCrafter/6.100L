b64e28df252a76e153592f5d6945f9c4e7cd6b0c
nixCrafter <nixCrafter.dev@gmail.com>
2025-11-17 05:37:17 +0700

    Complete Problem Set 3, Similarity

--- ps3/document_distance.py
+++ ps3/document_distance.py
@@ -100,7 +100,24 @@ def calculate_similarity_score(freq_dict1, freq_dict2):
          all frequencies in both dict1 and dict2.
         Return 1-(DIFF/ALL) rounded to 2 decimal places
     """
-    pass
+    keys = list(freq_dict1.keys())
+    for key in freq_dict2.keys():
+        if key not in keys:
+            keys.append(key)
+
+    diff = 0
+    all = 0
+    for key in keys:
+        if key in freq_dict1 and key in freq_dict2:
+            diff += abs(freq_dict1[key] - freq_dict2[key])
+            all += freq_dict1[key] + freq_dict2[key]
+        elif key not in freq_dict2:
+            diff += freq_dict1[key]
+            all += freq_dict1[key]
+        else:
+            diff += freq_dict2[key]
+            all += freq_dict2[key]
+    return round(1 - (diff / all), 2)
 
 
 ### Problem 4: Most Frequent Word(s) ###
@@ -202,28 +219,31 @@ if __name__ == "__main__":
     # print(friend_word_freq)  # should print {'hello': 1, 'friends': 1}
 
     ## Tests Problem 2: Get Letter Frequencies
-    freq1 = get_letter_frequencies("hello")
-    freq2 = get_letter_frequencies("that")
-    print(freq1)  #  should print {'h': 1, 'e': 1, 'l': 2, 'o': 1}
-    print(freq2)  #  should print {'t': 2, 'h': 1, 'a': 1}
+    # freq1 = get_letter_frequencies("hello")
+    # freq2 = get_letter_frequencies("that")
+    # print(freq1)  #  should print {'h': 1, 'e': 1, 'l': 2, 'o': 1}
+    # print(freq2)  #  should print {'t': 2, 'h': 1, 'a': 1}
 
     ## Tests Problem 3: Similarity
-    # test_directory = "tests/student_tests/"
-    # hello_world, hello_friend = load_file(test_directory + 'hello_world.txt'), load_file(test_directory + 'hello_friends.txt')
-    # world, friend = text_to_list(hello_world), text_to_list(hello_friend)
-    # world_word_freq = get_frequencies(world)
-    # friend_word_freq = get_frequencies(friend)
-    # word1_freq = get_letter_frequencies('toes')
-    # word2_freq = get_letter_frequencies('that')
-    # word3_freq = get_frequencies('nah')
-    # word_similarity1 = calculate_similarity_score(word1_freq, word1_freq)
-    # word_similarity2 = calculate_similarity_score(word1_freq, word2_freq)
-    # word_similarity3 = calculate_similarity_score(word1_freq, word3_freq)
-    # word_similarity4 = calculate_similarity_score(world_word_freq, friend_word_freq)
-    # print(word_similarity1)       # should print 1.0
-    # print(word_similarity2)       # should print 0.25
-    # print(word_similarity3)       # should print 0.0
-    # print(word_similarity4)       # should print 0.4
+    test_directory = "tests/student_tests/"
+    hello_world, hello_friend = (
+        load_file(test_directory + "hello_world.txt"),
+        load_file(test_directory + "hello_friends.txt"),
+    )
+    world, friend = text_to_list(hello_world), text_to_list(hello_friend)
+    world_word_freq = get_frequencies(world)
+    friend_word_freq = get_frequencies(friend)
+    word1_freq = get_letter_frequencies("toes")
+    word2_freq = get_letter_frequencies("that")
+    word3_freq = get_frequencies("nah")
+    word_similarity1 = calculate_similarity_score(word1_freq, word1_freq)
+    word_similarity2 = calculate_similarity_score(word1_freq, word2_freq)
+    word_similarity3 = calculate_similarity_score(word1_freq, word3_freq)
+    word_similarity4 = calculate_similarity_score(world_word_freq, friend_word_freq)
+    print(word_similarity1)  # should print 1.0
+    print(word_similarity2)  # should print 0.25
+    print(word_similarity3)  # should print 0.0
+    print(word_similarity4)  # should print 0.4
 
     ## Tests Problem 4: Most Frequent Word(s)
     # freq_dict1, freq_dict2 = {"hello": 5, "world": 1}, {"hello": 1, "world": 5}
